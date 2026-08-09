66c479df4a2a64aec5648b4e4a7fcce72906cc30
akash <git_ak@kuester-data.de>
2026-08-09 19:07:47 +0200

    Closes #3 - Dictionary fuer Gehaelter hinzugefuegt

---
+++ snippets/dictionary_gehaelter.py
@@ -0,0 +1,10 @@
+# Issue #3: Dictionary mit Mitarbeitern und Gehältern
+
+gehaelter = {
+    "Anna": 45000,
+    "Ben": 52000,
+    "Clara": 48000
+}
+
+# Gehalt von Ben ausgeben
+print(f"Das Gehalt von Ben beträgt: {gehaelter['Ben']} €")
